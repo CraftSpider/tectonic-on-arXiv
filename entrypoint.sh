4690d5ed28905f4b559f87bc18d97198d80050e8
Rune Tynan <runetynan@gmail.com>
2026-05-01 09:31:52 -0700

    Set default Rust

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # /github/workspace is a bind mount and might have wonky uids that scare modern git versions
 git config --global --add safe.directory "*"
+rustup default stable
 cd /root/github-ci
 yarn start
