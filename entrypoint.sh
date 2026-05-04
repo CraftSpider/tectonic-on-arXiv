b3682c6a2ed9e6631e103b5113ad358331ceb9a1
Rune Tynan <runetynan@gmail.com>
2026-05-04 16:49:16 -0700

    Fix syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 git config --global --add safe.directory "*"
 rustup default stable
 
-if [ -n "$2" ] then
+if [ -n "$2" ]; then
 #  export GH_TOKEN="$GITHUB_TOKEN"
 #  workflow_id=$(gh api repos/tectonic-typesetting/tectonic/actions/artifacts \
 #    --jq ".artifacts[] | select(.workflow_run.head_sha == \"$2\") | select(.name == \"tectonic-on-arxiv\") | .workflow_run.id")
-#  if [ -z "$2" ] then
+#  if [ -z "$2" ]; then
 #    run_url=gh workflow run tectonic-on-arxiv.yml -f head="$2"
 #    gh run watch ${run_url##*/}
 #  fi
