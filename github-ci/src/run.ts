429737189fda59b85129efbbcc6cb39f57dc48e1
Rune Tynan <runetynan@gmail.com>
2026-07-28 12:58:29 -0700

    Run report_ci in workspace

--- github-ci/src/run.ts
+++ github-ci/src/run.ts
@@ -93,7 +93,9 @@ export async function run_check({head_sha, head_branch, base_sha, check_run_id}:
         }, 15000)
 
         console.log("starting report_ci.py")
-        let proc = spawn("python3", ["/root/report_ci.py", `${workspace()}/datasets/${PR_RUN_DATASET}`, workspace(), head_sha])
+        let proc = spawn("python3", ["/root/report_ci.py", `${workspace()}/datasets/${PR_RUN_DATASET}`, workspace(), head_sha], {
+            "cwd": workspace(),
+        })
         proc.on("message", (msg) => {
             console.log("message", msg)
         })
